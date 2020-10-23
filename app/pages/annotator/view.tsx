fec787f3020988e0e0c6cfd976c8575ba7354dc1
Jan Slifka <jan.slifka@gmail.com>
2020-10-23 18:06:13 +0200

    Add annotator page

--- app/pages/annotator/view.tsx
+++ app/pages/annotator/view.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
-import type { SysContext, AppContext } from "app/context";
+import * as React from 'react'
+import type { SysContext, AppContext } from 'app/context'
+import { FormEvent } from 'react'
 
 interface Props {
   sysContext: SysContext;
@@ -7,9 +8,57 @@ interface Props {
 }
 
 export default function AnnotatorPage(props: Props): React.FunctionComponentElement<Props> {
+  const [pageUrlInputValue, setPageUrlInputValue] = React.useState('')
+  const [pageUrl, setPageUrl] = React.useState('')
+
+  function load(event: FormEvent) {
+    event.preventDefault()
+
+    if (pageUrlInputValue.length > 0) {
+      const annotateUrl = `${props.sysContext.config.apiServerUrl}${props.sysContext.config.apiPath}/annotator?url=${encodeURIComponent(pageUrlInputValue)}`
+      setPageUrl(annotateUrl)
+    }
+  }
+
+  function renderNavigation() {
+    return (
+      <form className="form-inline" onSubmit={load}>
+        <div className="form-group">
+          <input
+            type="text"
+            value={pageUrlInputValue}
+            onChange={event => setPageUrlInputValue(event.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button type="submit" onClick={load} className="btn btn-primary">Load</button>
+      </form>
+    )
+  }
+
+  function renderPage() {
+    return ( <iframe src={pageUrl} />)
+  }
+
+  function renderAnnotator() {
+    return (<span>Annotator</span>)
+  }
+
   return (
-    <div className="container-fluid">
-      <img src="https://marcellusdrilling.com/wp-content/uploads/2016/10/under-construction.png"/>
+    <div className="container-fluid annotator">
+      <div className="row row-bar bg-light border-bottom">
+        <div className="col">
+          {renderNavigation()}
+        </div>
+      </div>
+      <div className="row row-page">
+        <div className="col col-9">
+          {renderPage()}
+        </div>
+        <div className="col col-3 bg-light">
+          {renderAnnotator()}
+        </div>
+      </div>
     </div>
-  );
+  )
 }

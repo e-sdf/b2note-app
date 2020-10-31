c104273bf5936e284996e8dba61c19c8976e3a6d
Robert Pergl <robert@pergl.org>
2020-10-31 12:09:33 +0100

    correct image path

--- app/config.ts
+++ app/config.ts
@@ -16,7 +16,7 @@ export const config: ConfRec = {
   apiServerUrl: confRec?.apiServerUrl ? confRec.apiServerUrl : "http://localhost:3060",
   apiPath: confRec?.apiPath ? confRec.apiPath : "/api",
   solrUrl: confRec?.solrUrl ? confRec.solrUrl : "https://b2note.eudat.eu/solr/b2note_index/select",
-  imgPath: "/app/img/",
+  imgPath: "/img/",
   name: "B2NOTE Central UI",
   version: "v0.1.0",
   homepage: "https://b2note.bsc.es"

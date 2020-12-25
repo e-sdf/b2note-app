37a027acda98e00b167e7dc5317331b90aa920d1
Robert Pergl <robert@pergl.org>
2020-12-25 15:32:03 +0100

    update packages

--- app/config.ts
+++ app/config.ts
@@ -15,13 +15,16 @@ const confRec = (window as any).b2note as ConfRec|undefined;
 export const config: ConfRec = {
   appServerUrl: confRec?.appServerUrl ? confRec.appServerUrl : "",
   apiServerUrl: confRec?.apiServerUrl ? confRec.apiServerUrl : "http://localhost:3060",
-  widgetUrl: confRec?.widgetUrl ? confRec.widgetUrl : "https://b2note-dev.bsc.es/widget",
+  widgetUrl: confRec?.widgetUrl ? confRec.widgetUrl : "http://localhost:3061/widget",
   apiPath: confRec?.apiPath ? confRec.apiPath : "/api",
   solrUrl: confRec?.solrUrl ? confRec.solrUrl : "https://b2note.eudat.eu/solr/b2note_index/select",
   imgPath: "/img/",
   name: "B2NOTE Central UI",
-  version: "v0.4.0",
+  version: "v0.4.1",
   homepage: "https://b2note.bsc.es"
 };
+
+console.log("[B2NOTE-APP] Config:");
+console.log(config);
 
 export const endpointUrl = config.apiServerUrl + config.apiPath;

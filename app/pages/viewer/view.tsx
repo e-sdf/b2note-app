0ee822ec3ebdd66411eb6d64d2b21ad4294d049f
Robert Pergl <robert@pergl.org>
2021-01-07 19:51:58 +0100

    release

--- app/pages/viewer/view.tsx
+++ app/pages/viewer/view.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import DOMPurify from "dompurify";
 import ResizeObserver from "rc-resize-observer";
+import reactParse from "html-react-parser";
+import Alert from "app/components/alert";
 
 interface Props {
   imageUrl: string;
@@ -7,18 +10,42 @@ interface Props {
 }
 
 export default function ViewerPage(props: Props): React.FunctionComponentElement<Props> {
+  const [size, setSize] = React.useState([0, 0]);
+  const [svg, setSvg] = React.useState(null as string|null)
+  const [errorMessage, setErrorMessage] = React.useState(null as string|null);
+
+  React.useEffect(
+    () => {
+      const pureSvg = DOMPurify.sanitize(props.svg);
+      const parser = new DOMParser();
+      const svg = parser.parseFromString(pureSvg, "image/svg+xml").firstChild;
+      if (!svg || svg.nodeName !== "svg") {
+        setErrorMessage("Cannot display selection: not SVG format");
+      } else {
+        const rect = svg.firstChild;
+        if (!rect || rect.nodeName !== "rect") {
+          setErrorMessage("Cannot display selection: SVG does not contain <rect>");
+        } else {
+          setSvg(pureSvg);
+        }
+      }
+    },
+    [props.svg]
+  );
+
   return (
     <div className="container-fluid">
-      <svg id="image" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        <g>
-          <ResizeObserver
-            onResize={() => {
-              console.log("resized!");
-            }}>
-              <image xlinkHref={props.imageUrl}/>
-          </ResizeObserver>
-        </g>
-      </svg>
+      <Alert type="danger" message={errorMessage} closedHandler={() => setErrorMessage(null)} />
+      {svg ?
+        <svg id="image" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width={size[0]} height={size[1]}>
+          <g>
+            <ResizeObserver onResize={dimensions => setSize([dimensions.width, dimensions.height])}>
+                <image xlinkHref={props.imageUrl}/>
+            </ResizeObserver>
+            {reactParse(svg)}
+          </g>
+        </svg>
+      : <></>}
     </div>
   );
 }
